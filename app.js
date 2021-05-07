03e586d72e34e8fcfdb9204b030cef33dd4c3fde
GiordanDelaCruz <GiordanDelaCruz@gmail.com>
2021-05-07 17:13:59 -0400

    Updated backend code and updated UI look.

--- app.js
+++ app.js
@@ -5,45 +5,43 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Declare Global variables
+let items = ["Morning Routine", "Eat Breakfast"];
+
+// Homepage
 app.get("/", function(req, res){
 
   // Retreive the day of the week
-  var d = new Date();
-  var day =  d.getDay();
-  var strDayOfWeek;
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  };
 
-  if( day == 0){
-    // Sunday
-    strDayOfWeek = "Sunday";
-
-  }else if (day == 1){
-    // Monday
-    strDayOfWeek = "Monday";
-
-  }else if (day == 2){
-    // Tuesday
-    strDayOfWeek = "Tuesday";
-
-  }else if (day == 3){
-    // Wednesday
-    strDayOfWeek = "Wednesday";
-
-  }else if (day == 4){
-    // Thursday
-    strDayOfWeek = "Thursday";
-
-  }else if (day == 5){
-    // Friday
-    strDayOfWeek = "Friday";
-
-  }else if (day == 6){
-    // Saturday
-    strDayOfWeek = "Saturday";
-  }
+  // Convert today's date into a nice String
+  let day = today.toLocaleDateString("en-US", options);
 
   // Send the day of the week to the ejs template
-  res.render("dayOfWeek", {dayOfWeek : strDayOfWeek} );
+  res.render("list", {dayOfWeek : day, listItems : items } );
+});
+
+
+// Handle POST requests on list.ejs
+app.post("/", function(req, res){
+  var item = req.body.todoItem;
+
+  // Add item to the items array
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
